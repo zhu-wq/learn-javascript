c84ebdda9414513a7a04dea5992cce91f84d20d1
zhu-wq <zhu.wenqiang@outlook.com>
2017-03-31 15:12:33 +0800

    添加对于NODE_ENV的注释

--- samples/node/web/koa/view-koa/app.js
+++ samples/node/web/koa/view-koa/app.js
@@ -8,6 +8,9 @@ const templating = require('./templating');
 
 const app = new Koa();
 
+//Node.js在全局变量process中定义了一个环境变量env.NODE_ENV
+//开发的时候环境变量应该设置为'development'或空，而部署到服务器时，环境变量应该设置为'production'。
+//在编写代码的时候，要根据当前环境作不同的判断。
 const isProduction = process.env.NODE_ENV === 'production';
 
 // log request URL:
@@ -22,6 +25,9 @@ app.use(async (ctx, next) => {
 });
 
 // static file support:
+//这是因为在生产环境下，静态文件是由部署在最前面的反向代理服务器（如Nginx）处理的，
+//Node程序不需要处理静态文件。而在开发环境下，我们希望koa能顺带处理静态文件，
+//否则，就必须手动配置一个反向代理服务器，这样会导致开发环境非常复杂。
 if (! isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
