385246959d66cc574460d515e22a512a85f8dd8c
zhu-wq <zhu.wenqiang@outlook.com>
2017-04-06 13:45:54 +0800

    Update app.js
    Comment out the try catch blocks

--- samples/node/web/db/model-sequelize/app.js
+++ samples/node/web/db/model-sequelize/app.js
@@ -29,14 +29,14 @@ let
 })();
 
 (async () => {
-    try {
-        var pets = await Pet.findAll();// 这里得到了一个返回错误
-    } catch (err) {
-        console.log(err); // 这里捕捉到错误 `error`
-        return;
-    }
+//     try {
+//         var pets = await Pet.findAll();// 这里得到了一个返回错误
+//     } catch (err) {
+//         console.log(err); // 这里捕捉到错误 `error`
+//         return;
+//     }
 
-    // var pets = await Pet.findAll();
+    var pets = await Pet.findAll();
     for(var i=0; i< pets.length; i++) {
         var pet = pets[i];
         console.log("sqlQueryResult >>> " + JSON.stringify(pet));
